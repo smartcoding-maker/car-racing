a37d09b6ceec83234b6a0f6e351b891263dd7230
smartcoding-maker <76058277+smartcoding-maker@users.noreply.github.com>
2021-02-19 19:03:04 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,10 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+       var title=createElement('h1')
+       title.html("multiPlayer car racing game")
+       title.position(420,0)
+    }
+}
